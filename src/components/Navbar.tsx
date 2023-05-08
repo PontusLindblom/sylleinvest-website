fbb75f784bdde3c33e0576430e3c3b9dea165ecd
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-05-08 14:43:47 +0200

    Add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,26 @@
-﻿const Navbar = () => {
+﻿import styles from './navbar.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => {
   return(
-    <h1>Navbar</h1>
+    <div className={styles.navbarContainer}>
+        <Link href="/" className={styles.navbarLink}>
+            <Image src="/navbar-logo.webp" alt="SylleInvest Logotype" width={150} height={64}/>
+        </Link>
+
+        <Link href="/about" className={styles.navbarLink}>
+            About
+        </Link>
+
+        <Link href="/resources" className={styles.navbarLink}>
+            Resources
+        </Link>
+
+        <Link href="/contact" className={styles.navbarLink}>
+            Contact
+        </Link>
+    </div>
   )
 }
 
