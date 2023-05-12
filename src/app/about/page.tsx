725c7ef92dfd6a9d089d1894b4df58f5421b9b77
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-05-12 11:03:50 +0200

    Add about page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,33 @@
-﻿export default function About() {
+﻿import styles from "./page.module.css"
+import {FaBook, FaChartLine, FaBullseye} from 'react-icons/fa';
+
+export default function About() {
     return (
-        <h1>About page</h1>
+        <div>
+            <h1>About Sylle Invest</h1>
+            <div className={styles.paragraphContainer}>
+                <FaBook className={styles.icon}/>
+                <p>
+                    <strong>My Story</strong><br/>
+                    Lorem ipsum!
+                </p>
+            </div>
+
+            <div className={styles.paragraphContainer}>
+                <FaBullseye className={styles.icon}/>
+                <p>
+                    <strong>My Mission</strong><br/>
+                    Spread knowledge!
+                </p>
+            </div>
+
+            <div className={styles.paragraphContainer}>
+                <FaChartLine className={styles.icon}/>
+                <p>
+                    <strong>Track Record</strong><br/>
+                    Very success, much wow!
+                </p>
+            </div>
+        </div>
     )
 }
