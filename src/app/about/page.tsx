e52eac33e791afee7f1e9ac1a55e81eca6de1dfd
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-05-22 16:13:10 +0200

    Refactor to a better code structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,32 +1,44 @@
 ﻿import styles from "./page.module.css"
 import {FaBook, FaChartLine, FaBullseye} from 'react-icons/fa';
 
-export default function About() {
+export default function Page() {
     return (
         <div>
             <h1>About Sylle Invest</h1>
             <div className={styles.paragraphContainer}>
                 <FaBook className={styles.icon}/>
-                <p>
-                    <strong>My Story</strong><br/>
-                    Lorem ipsum!
-                </p>
+                <div>
+                    <h3>
+                        My Story
+                    </h3>
+                    <p>
+                        Lorem ipsum!
+                    </p>
+                </div>
             </div>
 
             <div className={styles.paragraphContainer}>
                 <FaBullseye className={styles.icon}/>
-                <p>
-                    <strong>My Mission</strong><br/>
-                    Spread knowledge!
-                </p>
+                <div>
+                    <h3>
+                        My Mission
+                    </h3>
+                    <p>
+                        Spread knowledge!
+                    </p>
+                </div>
             </div>
 
             <div className={styles.paragraphContainer}>
                 <FaChartLine className={styles.icon}/>
-                <p>
-                    <strong>Track Record</strong><br/>
-                    Very success, much wow!
-                </p>
+                <div>
+                    <h3>
+                        Track Record
+                    </h3>
+                    <p>
+                        Such success, much wow!
+                    </p>
+                </div>
             </div>
         </div>
     )
--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,5 +1,7 @@
-﻿export default function Resources() {
+﻿export default function Page() {
     return (
-        <h1>Resources page</h1>
+        <div>
+            <h1>Resources Page</h1>
+        </div>
     )
 }
