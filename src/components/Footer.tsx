337278a5fb6ae8d48b8bb8c337c57467471639c5
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-05-23 15:31:26 +0200

    Add footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,33 @@
 ﻿import styles from './footer.module.css'
+import Link from "next/link";
+import {FaYoutube, FaTwitter, FaPatreon} from "react-icons/fa";
 
 const Footer = () => {
     return (
         <div className={styles.footerContainer}>
-            <div className={styles.footerText}>Placeholder</div>
-            <div className={styles.footerText}>Placeholder</div>
-            <div className={styles.footerText}>Placeholder</div>
+            <div className={styles.footerItem}>
+                <strong>Sylle Invest</strong>
+                <div className={styles.socialIcons}>
+                    <Link href={"https://www.youtube.com/sylleinvest"}>
+                        <FaYoutube aria-label={"YouTube Logo"}/>
+                    </Link>
+                    <Link href={"https://twitter.com/sylleinvest"}>
+                        <FaTwitter aria-label={"Twitter Logo"}
+                                   className={styles.marginInline}/>
+                    </Link>
+                    <Link href={"https://www.patreon.com/sylleinvest"}>
+                        <FaPatreon aria-label={"Patreon Logo"}/>
+                    </Link>
+                </div>
+                <p>Brings knowledge to investing</p>
+            </div>
+
+            <div className={styles.footerItem}>
+                <strong>Disclaimer</strong>
+                <p>I am no financial advisor</p>
+                <p>Only spend what you can afford to lose</p>
+                <p>The website is for informational purposes only</p>
+            </div>
         </div>
     )
 }
