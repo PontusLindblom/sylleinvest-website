a74b606ecde4d76b91813a66722c5f940e5401e3
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-05-29 09:01:00 +0200

    Add responsive design to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,66 @@
-﻿import styles from './navbar.module.css'
+﻿"use client";
+
+import styles from './navbar.module.css'
 import Image from 'next/image';
 import Link from 'next/link';
+import {FaBars, FaTimes} from "react-icons/fa";
+import {useState} from "react";
 
 const Navbar = () => {
+    const [dropdown, setDropdown] = useState(false);
+
     return (
-        <div className={styles.navbarContainer}>
-            <Link href="/" className={styles.navbarLogo}>
-                <Image src="/navbar-logo.webp" alt="Sylle Invest Logotype" width={150} height={64} priority={true}/>
-            </Link>
+        <div
+            className={`${dropdown ? styles.navbarContainerExtended : styles.navbarContainer} ${styles.limitHeightIfWide}`}>
+            <div className={styles.navbarMenu}>
+                <Link href="/" className={styles.navbarLogo}>
+                    <Image src="/navbar-logo.webp"
+                           alt="Sylle Invest Logotype"
+                           width={150}
+                           height={64}
+                           priority={true}/>
+                </Link>
 
-            <Link href="/about" className={styles.navbarItem}>
-                About
-            </Link>
+                <Link href="/about"
+                      className={`${styles.navbarItem} ${styles.hideIfNarrow}`}>
+                    About
+                </Link>
 
-            <Link href="/resources" className={styles.navbarItem}>
-                Resources
-            </Link>
+                <Link href="/resources"
+                      className={`${styles.navbarItem} ${styles.hideIfNarrow}`}>
+                    Resources
+                </Link>
 
-            <Link href="/contact" className={styles.navbarItem}>
-                Contact
-            </Link>
+                <Link href="/contact"
+                      className={`${styles.navbarItem} ${styles.hideIfNarrow}`}>
+                    Contact
+                </Link>
+
+                <button className={`${styles.dropdownBtn} ${styles.hideIfWide}`}
+                        onClick={() => {
+                            setDropdown((dropdown: boolean) => !dropdown);
+                        }}>
+                    {dropdown ? <FaTimes/> : <FaBars/>}
+                </button>
+            </div>
+
+            <div
+                className={`${dropdown ? styles.navbarDropdown : styles.hide} ${styles.hideIfWide}`}>
+                <Link href="/about"
+                      className={`${styles.navbarItemDropdown}`}>
+                    About
+                </Link>
+
+                <Link href="/resources"
+                      className={`${styles.navbarItemDropdown}`}>
+                    Resources
+                </Link>
+
+                <Link href="/contact"
+                      className={`${styles.navbarItemDropdown}`}>
+                    Contact
+                </Link>
+            </div>
         </div>
     )
 }
