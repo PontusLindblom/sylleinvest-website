d432148e062558bad1ec27239aa134c06f35a62b
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-05-30 11:14:58 +0200

    Fix nestling hierarchy for availability

--- src/app/blog/exam-and-new-job/page.tsx
+++ src/app/blog/exam-and-new-job/page.tsx
@@ -6,10 +6,10 @@ export default function Page() {
             <h1>Exam, new job, video release!</h1>
             <div className={styles.publishDate}>Published 2022-08-28</div>
 
-            <h3>
+            <h2>
                 Exam - I&#39;ve become a civil engineer with a Master&#39;s in
                 Computer Science!
-            </h3>
+            </h2>
             <p>
                 After six years of school, I&#39;ve finally become a civil
                 engineer! As a person who loves working, this is a huge
@@ -18,10 +18,10 @@ export default function Page() {
                 (which is almost impossible as a student).
             </p>
 
-            <h3>
+            <h2>
                 New job - I will start working as an IT-consultant starting
                 tomorrow!
-            </h3>
+            </h2>
             <p>
                 Things move fast. Become a civil engineer on Tuesday, and start
                 working as an IT consultant the following Monday. I still
@@ -30,10 +30,10 @@ export default function Page() {
                 scent of adventure!
             </p>
 
-            <h3>
+            <h2>
                 Video release - My mid-year review of Quantum eMotion is
                 published!
-            </h3>
+            </h2>
             <p>
                 Before running away (and being overwhelmed) by the new job, I
                 managed to release my mid-year review of Quantum eMotion.
