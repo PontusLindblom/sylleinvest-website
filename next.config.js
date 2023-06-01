065b85820e57042be484719c218774a815df5cae
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-06-01 21:56:17 +0200

    Add reactStrictMode
    Adding reactStrictMode will simplify the development experience as it
    highlights potential problems in the application.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
+    // Specify that we use the app-dir structure, not the page-dir structure
+    experimental: {appDir: true},
+
+    // Next.js image optimization doesn't work with "output: 'export'"
+    images: {unoptimized: true},
+
+    // Build as a static website
+    output: 'export',
+
+    // Highlight potential problems in the application
+    reactStrictMode: true
 }
 
 module.exports = nextConfig
