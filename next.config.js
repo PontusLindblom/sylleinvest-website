3daefcd3e69262ade7c30d88210f341e06703848
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-06-15 22:34:16 +0200

    Remove deprecated code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Specify that we use the app-dir structure, not the page-dir structure
-    experimental: {appDir: true},
-
     // Build as a static website
     output: 'export',
 
