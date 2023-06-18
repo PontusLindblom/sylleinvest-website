c35fd9a8f60d17b4cb8e993701aded43c746ad65
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-06-18 21:55:37 +0200

    Redesign the Blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,47 +6,55 @@ export default function Home() {
         <div>
             <h1>Blog Updates</h1>
 
-            <Link href="/blog/summary-of-lux-strategy-call/"
-                  className={styles.blogPost}>
-                <div className={styles.publishDate}>Mar 31<br/>2023</div>
-                <div className={styles.lineSeparator}></div>
-                <div className={styles.headline}>
+            <Link href="/blog/summary-of-lux-strategy-call/" className={styles.blogPost}>
+                <h2>
                     Summary of Newlox’s Strategy Call
-                </div>
+                </h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.summary}>
+                    A call where the company discuss their strategy for the 2023 fiscal year.
+                </p>
             </Link>
 
             <Link href="/blog/exam-and-new-job/" className={styles.blogPost}>
-                <div className={styles.publishDate}>Aug 28<br/>2022</div>
-                <div className={styles.lineSeparator}></div>
-                <div className={styles.headline}>
+                <h2>
                     Exam, New Job, Video Release
-                </div>
+                </h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.summary}>
+                    An update on what is going on in my life right now.
+                </p>
             </Link>
 
-            <Link href="/blog/summary-of-lux-zoom-call/"
-                  className={styles.blogPost}>
-                <div className={styles.publishDate}>July 23<br/>2022</div>
-                <div className={styles.lineSeparator}></div>
-                <div className={styles.headline}>
+            <Link href="/blog/summary-of-lux-zoom-call/" className={styles.blogPost}>
+                <h2>
                     Summary of Newlox’s Zoom Call
-                </div>
+                </h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.summary}>
+                    A call on July 19th, 2022, where the company provide updates for their shareholders.
+                </p>
             </Link>
 
-            <Link href="/blog/interview-with-lux-ceo/"
-                  className={styles.blogPost}>
-                <div className={styles.publishDate}>May 12<br/>2022</div>
-                <div className={styles.lineSeparator}></div>
-                <div className={styles.headline}>
+            <Link href="/blog/interview-with-lux-ceo/" className={styles.blogPost}>
+                <h2>
                     Interview With CEO of Newlox Gold
-                </div>
+                </h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.summary}>
+                    A detailed interview where we discuss the company’s progress and take a deep dive into their ramp-up
+                    process.
+                </p>
             </Link>
 
             <Link href="/blog/website-launched/" className={styles.blogPost}>
-                <div className={styles.publishDate}>Apr 19<br/>2022</div>
-                <div className={styles.lineSeparator}></div>
-                <div className={styles.headline}>
+                <h2>
                     Sylleinvest.com Website Launched
-                </div>
+                </h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.summary}>
+                    After some dedicated work creating this website from scratch, it is finally live!
+                </p>
             </Link>
         </div>
     )
