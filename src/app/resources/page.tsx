66c24f0da273ce74d2c2e655bde35d1e381bf8bc
Pontus Lindblom <pontus.lindblom@omegapoint.se>
2023-06-22 13:58:53 +0200

    Add placeholder design to resource page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,9 +1,22 @@
 ﻿import styles from "./page.module.css"
+import Link from "next/link";
 
 export default function Page() {
     return (
         <div>
-            <h1>This Section Is Under Construction</h1>
+            <h1>Research Resources</h1>
+
+            <Link href="/resources/placeholder" className={styles.resourceContainer}>
+                <h2>Under Construction</h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.tickerSymbols}>Ticker: XX, Ticker: XX, Ticker: XX</p>
+            </Link>
+
+            <Link href="/resources/placeholder" className={styles.resourceContainer}>
+                <h2>Under Construction</h2>
+                <div className={styles.lineSeparator}/>
+                <p className={styles.tickerSymbols}>Ticker: XX, Ticker: XX, Ticker: XX</p>
+            </Link>
         </div>
     )
 }
---
+++ src/app/resources/placeholder/page.tsx
@@ -0,0 +1,9 @@
+﻿import styles from '../page.module.css'
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Under Construction</h1>
+        </div>
+    )
+}
